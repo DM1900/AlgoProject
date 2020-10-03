603737dd0e7f3eb006bbe3ba3b4eb913c858f305
Derek <frwd00@gmail.com>
2020-10-03 19:51:02 +0100

    Added changes to work on Windows

--- ProjectFile_02.py
+++ ProjectFile_02.py
@@ -13,23 +13,27 @@ pip install datetime
 """
 
 # imports:
+print("Load imports")
 from pandas_datareader import data
 from pandas_datareader._utils import RemoteDataError
 import matplotlib as plt
 import matplotlib.pyplot as pyplt
 import pandas as pd
 import numpy as np
+
 from datetime import datetime, timedelta
 import csv
+
 #
-PastPrice = 28 # no. of days data to gather
-START_DATE = str((datetime.today()- timedelta(days=PastPrice)).strftime('%Y-%m-%d'))
+print("Load variables")
+PriceHistory = 28 # no. of days data to gather
+START_DATE = str((datetime.today()- timedelta(days=PriceHistory)).strftime('%Y-%m-%d'))
 END_DATE = str(datetime.now().strftime('%Y-%m-%d'))
 tickerlist = "tickerfile.txt"
 with open(tickerlist) as file:
     tickers = [ticker.rstrip('\n') for ticker in file]
-#tickers = ['AAPL']
-
+tickers = ['AAPL','AMZN']
+"""
 def get_stats(stock_data):
     return {
         'last': np.mean(stock_data.tail(1)),
@@ -38,12 +42,12 @@ def get_stats(stock_data):
         'short_rolling': stock_data.rolling(window=20),
         'long_rolling': stock_data.rolling(window=200)
     }
-
+"""
 def clean_data(stock_data,col):
     weekdays = pd.date_range(start=START_DATE, end=END_DATE)
     clean_data = stock_data[col].reindex(weekdays)
     return clean_data.fillna(method='ffill')
-
+"""
 def create_plot(stock_data, ticker):
     stats = get_stats(stock_data)
     pyplt.subplots(figsize=(12,8))
@@ -53,7 +57,7 @@ def create_plot(stock_data, ticker):
     pyplt.legend()
     pyplt.title('Stock ticker')
     pyplt.show()
-
+"""
 def get_data(ticker):
     try:
         stock_data = data.DataReader(ticker,'yahoo',START_DATE,END_DATE)
@@ -63,22 +67,22 @@ def get_data(ticker):
         RSI_PERIOD = 14
         chg = df['Adj Close'].diff(1)
         gain = chg.mask(chg<0,0)
-        #df['gain'] = gain
         loss = chg.mask(chg>0,0)
-        #df['loss'] = loss
         #
         avg_gain = gain.ewm(com=RSI_PERIOD-1,min_periods=RSI_PERIOD).mean()
         avg_loss = loss.ewm(com=RSI_PERIOD-1,min_periods=RSI_PERIOD).mean()
         #
         RS = abs(avg_gain / avg_loss)
         RSI = 100 - (100/(1+RS))
+        df['Date'] = END_DATE
         df['Ticker'] = ticker
         df['RSI'] = RSI
         df = (df[-1:])
-        print(df[-1:])
+        FINALDATA = df['Date'],df['Ticker'],df['RSI']
+        print(FINALDATA)
         #print("Stock:",ticker,RSI[-1:])
-        CSV_FILE = "output/csvfile.csv"
-        df.to_csv(CSV_FILE,index=False)
+        #CSV_FILE = "output/csvfile_20201003.csv"
+        #df.to_csv(CSV_FILE,index=False)
         #to_clipboard(excel=True,sep=None)
         #print(NEW_DATA)
         #f = open("newfile.txt", "a")
@@ -92,8 +96,5 @@ def get_data(ticker):
 
 for ticker in tickers:
     get_data(ticker)
-
-
-
 
 #end
