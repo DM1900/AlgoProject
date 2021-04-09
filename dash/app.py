cbf230685db20d7ff184e2ad1f49ff02e5afa221
Derek Murphy <frwd00@gmail.com>
2021-04-09 21:06:47 +0100

    Additions to the dash script

--- dash/app.py
+++ dash/app.py
@@ -1,8 +1,14 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
+# dash modules
 import dash
+import dash
+import dash_core_components as dcc
 import dash_html_components as html
+import plotly.express as px
+# others
+from dash.dependencies import Input, Output
 import pandas as pd
 from datetime import datetime, timedelta
 from pandas_datareader import data
@@ -74,20 +80,95 @@ def generate_table(dataframe, max_rows=10):
         ])
     ])
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-#external_stylesheets = ['https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css']
+### Charts
+# Create your connection.
+DB_FOLDER = './scripts/db/' 
+DB_NAME = 'pnl.db'
+DB_NAME = '{}{}'.format(DB_FOLDER,DB_NAME) # this DB stores all account value data
+print(DB_NAME)
+TABLE_NAME = "pldata"
+connection = sqlite3.connect(DB_NAME)
+cursor = connection.cursor()
+# create datastore with data from db
+cmd = "SELECT * FROM {}".format(TABLE_NAME)
+data = pd.read_sql(cmd, connection) 
+#print(data)
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+def read_table(cmd):
+    global data
+    cursor.execute(cmd)
+    data = pd.read_sql(cmd, connection) 
+    #print(data.head())
+
+# gather all data
+cmd = "SELECT * FROM {}".format(TABLE_NAME)
+#read_table(cmd)
+
+# gather data from specific dates (or any specified column)
+col = "Date"
+var = "2021"
+cmd = "SELECT * FROM {} WHERE {} LIKE '%{}%'".format(TABLE_NAME,col,var)
+
+df2 = pd.read_sql(cmd, connection)
+
+#print(df2)
+#exit()
+###
+
+
+
+#df2 = px.data.gapminder().query("continent=='Oceania'")
+fig = px.line(df2, x="Date", y="TotalValue")
+
+
+###
+
+#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+#external_stylesheets = ['https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css']
+#external_stylesheets = ['https://codepen.io/chriddyp/pen/dZVMbK.css']
+
+app = dash.Dash(__name__)#,external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
     html.H4(children='Buy/Sell list'),
     generate_table(df0),
-    html.H6(children='---'),
+    html.Hr(),
     html.H4(children='Yearly Account Data'),
     generate_table(df2),
     html.H4(children='Account Data (Cumulative)'),
-    generate_table(df1)
+    generate_table(df1),
+    html.Hr(),
+    html.H4(children='Enter Data'),
+    # input
+    dcc.Input(id="Total", type="number", placeholder="Total account value"),
+    dcc.Input(id="Inv", type="number", placeholder="Total invested amount"),
+    dcc.Input(id="Real", type="number", placeholder="Total realised amount"),
+    dcc.Input(id="Div", type="number", placeholder="Total dividend amount"),
+    html.Div(id="number-out"),
+    # button
+    html.Button('Button 1', id='btn1', n_clicks=0),
+    html.Div(id='container-button-timestamp'),
+    dcc.Graph(figure=fig),
 ])
+
+@app.callback(
+    # button
+    Output('container-button-timestamp', 'children'),
+    Input('btn1', 'n_clicks'),
+    Input("Total", "value"),
+    Input("Inv", "value"),
+    Input("Real", "value"),
+    Input("Div", "value"),
+
+)
+def update_output(event,Total, Inv, Real, Div):
+    SUM = event #Total + Inv + Real + Div + event
+    return 'This is a test of the button {}, {}, {}, {}, {}, {}'.format(SUM,event,Total, Inv, Real, Div)
+    #SQLITE_func.EnterData(Total,Inv,Real,Div)
+
+
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
